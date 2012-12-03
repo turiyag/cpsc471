9e6842afa1d7384fd2fff4224c1cd816b13f7326
Mitchell Ludwig <mitchell.ludwig@gmail.com>
2012-12-03 08:15:46 +0000

    Commited working changes

---
+++ mobile/help.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include('head.php'); ?>
+    </head>
+    <body>
+        <div data-role="page" data-theme="a">
+            <?php include('header.php');?>
+            <div data-role="content">
+                <h3>This is the help page</h3>
+            <?php include('footer.php'); ?>
+        </div>
+    </body>
+</html>
