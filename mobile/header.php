52310e7a821d250c4788cb9e2aee87bb05d66bcf
Olabode Samuel Adegbayike <osadegba@ucalgary.ca>
2012-12-03 00:13:18 -0700

    more adjusment to help button

--- mobile/header.php
+++ mobile/header.php
@@ -1,5 +1,5 @@
 <div data-role="header">
     <h1>DNDN</h1>
     <a href="./" data-icon="home" data-iconpos="notext" data-direction="reverse">Home</a>
-	<a href="help" data-role="button" data-icon="" data-inline="true" data-mini="true" data-icon="info" class="ui=btn-right">Help</a>
+	<a href="help" data-role="button" data-icon="info" data-inline="true" data-mini="true" class="ui=btn-right" data-theme="b">Help</a>
 </div>
