cb75dfa7d8de5a01d4d1dcd4c4892e8178105771
Olabode Samuel Adegbayike <osadegba@ucalgary.ca>
2012-12-03 00:09:45 -0700

    added help button

--- mobile/header.php
+++ mobile/header.php
@@ -1,4 +1,5 @@
 <div data-role="header">
     <h1>DNDN</h1>
     <a href="./" data-icon="home" data-iconpos="notext" data-direction="reverse">Home</a>
+	<a href="help" data-role="button" data-icon="" data-inline="true" data-mini="true" data-icon="info" class="ui=btn-right">Help</a>
 </div>
